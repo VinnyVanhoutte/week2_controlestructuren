2350abec94bd2e5219b7eaffb760dd6a54b4b225
VinnyVanhoutte <vinny.vanhoutte@student.howest.be>
2017-10-02 11:11:26 +0200

    week 2 controle structuren

---
+++ oefeningen.py
@@ -0,0 +1,9 @@
+#oefeningen - controle structuren
+
+getalA = int(input("geef een getal"))
+getalB = int(input("geef een tweede getal"))
+
+if (getalA == getalB):
+    print("het getal {0} en het getal {1} zijn gelijk".format(getalA,getalB))
+else:
+    print("het getal {0} en het getal {1} zijn verschillend".format(getalA,getalB))
